e2eefd16f46e7ca101322f8d1eeaabd165be5eb0
Junzhe Zheng <jzheng9@hawk.iit.edu>
2014-02-18 00:31:18 -0600

    Add title in java file.

--- homework/homework3/CS430HW3_Quicksort.java
+++ homework/homework3/CS430HW3_Quicksort.java
@@ -1,10 +1,16 @@
-package cs430HW3;
 
 public class CS430HW3_Quicksort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("This program is a quicksort algorithms implementation.");
+		System.out.println("A list of random interger numbers will be atuomatic generated.");
+		System.out.println("Then quicksort method will be applied to the list.");
+		System.out.println("This procedure will be repeated FIVE times with a differnt size of list.");
+		System.out.println("List sizes are 2^15,2^17,2^19,2^21,2^23 and 2^25");
+		System.out.println("Time will be recorded during sorting process.");
+		System.out.println("For a certain size, sorting process will be repeated FIVE times to get the average sorting time.");
+		System.out.println("-----------------------------------------------");
 	}
 
 }
